53099d340bd507138bed16cc533745ebedb30f47
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-08-07 11:43:47 +0700

    create TaskStatus Component

---
+++ app/ui/tasks/TaskStatus.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+import { FaCircleCheck, FaClock } from "react-icons/fa6";
+
+const TaskStatus = ({ status }: { status: boolean }) => {
+	return (
+		<span
+			className={clsx(
+				"inline-flex items-center rounded-full px-2 py-1 text-xs",
+				{
+					"bg-green-200 text-green-800": status,
+					"bg-emerald-50 text-emerald-800": !status,
+				},
+			)}>
+			{status ? (
+				<>
+					<span className="hidden md:inline mr-1">Done</span>
+					<FaCircleCheck className="w-4" />
+				</>
+			) : (
+				<>
+					<span className="hidden md:inline mr-1">Not Yet</span>
+					<FaClock className="w-4" />
+				</>
+			)}
+		</span>
+	);
+};
+
+export default TaskStatus;
