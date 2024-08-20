5c66c545771cd12402a0abd7e40b1960a901ff12
Ahmad Elshowair <it.is.ahmad.elshowair@outlook.com>
2024-08-20 16:04:37 +0700

    "Refactor TaskLogo component and remove its usage from Home page"

--- app/components/TaskLogo.tsx
+++ app/components/TaskLogo.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { GoTasklist } from "react-icons/go";
 
 const TaskLogo = () => {
+	const pathname = usePathname();
 	return (
 		<div className="flex flex-row leading-none items-center">
 			<GoTasklist className="h-12 w-12 text-emerald-800" />
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,16 @@
 import { verifySession } from "@/app/lib/session";
 import Link from "next/link";
 import { BsArrowRight } from "react-icons/bs";
-import TaskLogo from "./components/TaskLogo";
 
 export default async function Home() {
 	const session = await verifySession();
 	return (
 		<section className="flex min-h-screen h-full p-4 w-full flex-col gap-3">
-			{/* THE LOGO SECTION  */}
-			<div className="flex items-end rounded-lg bg-gradient-to-tr from-neutral-400 via-slate-400 to-stone-400 p-4 md:h-[30%]">
-				<TaskLogo />
-			</div>
-
 			{/* THE WELCOME SECTION */}
-			<div className="flex flex-col gap-6 rounded-lg bg-neutral-50 px-6 py-10 md:px-20 shadow h-[70%] justify-center">
+			<div className="flex flex-col gap-6 rounded-lg bg-neutral-50 px-6 py-10 md:px-20 shadow h-full justify-center items-center">
 				<h1 className="text-3xl font-bold mb-4">Welcome to Task Manager</h1>
 				{session ? (
-					<div className="flex flex-col gap-12 w-8/12 ">
+					<div className="flex flex-col gap-12 w-8/12 items-center">
 						<p className="text-lg text-neutral-700 font-semibold capitalize">
 							You are logged in as{" "}
 							<span className="text-green-400 ml-4">{session?.user_name}</span>
